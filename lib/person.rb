4c72aa33b46a22619fa45126b2390f181ab16a1c
Daniel Calvin <arockne2003@yahoo.com>
2021-10-07 14:14:55 -0700

    commit tree 3541adaa80924611fe0cad024b0777379ea33957

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+#Person class
+  #accepts a hash on init
+   #each key should become a property of the Person instance
+     #also each attribute should be assigned a getter/setter
+
 class Person
-  # your code here
+  def initialize(**args)
+    args.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
